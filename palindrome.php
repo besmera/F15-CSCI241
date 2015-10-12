a87cef42ac8f568f43d5f5247a4ad52caea1a4d8
Andrew Besmer <besmera@winthrop.edu>
2015-10-12 12:09:52 -0400

    Class Examples

---
+++ palindrome.php
@@ -0,0 +1,32 @@
+<?php
+
+$word = "pool";
+
+$palStack = array();
+
+for($ct = 0; $ct < strlen($word); $ct++)
+{
+	array_push($palStack, $word[$ct]);
+}
+
+$isPal = true;
+
+for($ct = 0; $ct < strlen($word); $ct++)
+{
+	$currentValue = array_pop($palStack);
+	if($currentValue != $word[$ct])
+	{
+		$isPal = false;
+		break;
+	}
+}
+
+if($isPal == true)
+{
+	echo "This is a palindrome!";
+}
+else {
+	echo "This is not a palindrome!";
+}
+
+
